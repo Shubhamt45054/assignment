f86c0fd3dec7908a28d1592c976bd5e69eb2b8b6
project45054 <project45054@gmail.com>
2025-04-09 22:59:45 +0530

    Koyeb

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173']
+  origin: ['http://localhost:5173',"https://feedback-form-xyz.netlify.app/"]
 }));
 
 // json data
