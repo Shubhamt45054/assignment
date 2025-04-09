70c6639f736fd331b8bcf8901702fa0b25805272
project45054 <project45054@gmail.com>
2025-04-09 18:00:32 +0530

    setting up the database

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import connectDB from "./src/db/index.js";
+import dotenv from "dotenv";
+
+const app=express();
+
+dotenv.config();
+connectDB()
+.then(()=>{  
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(err);
+})
+
